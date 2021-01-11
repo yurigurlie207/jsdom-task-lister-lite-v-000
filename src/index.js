1c361dd4d48fd667523a346fb8597e749e36438e
Adrienne Yao <adrienne.yao@icloud.com>
2021-01-11 02:58:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //grab all the necessary DOM elements
+
+  //form and relevant input fields
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  //ul where new tasks will live on the DOM
+  const newTaskUl = document.getElementById("tasks");
+
+  //attach event listeners
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  //stop form from trying to submit
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
